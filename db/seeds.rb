e703e019f5497eb235bc5939a0069c34c30f572f
erika desu <erikadesu@gmx.com>
2021-07-22 15:12:27 -0400

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'net/http'
+require 'json'
+
+url = "https://api.themoviedb.org/3/movie/top_rated?api_key=#{Rails.application.credentials.tmdb[:api_key]}&language=en-US&page=1"
+response = Net::HTTP.get_response(URI(url))
+api_data = JSON.parse(response.body)
+
+puts "planting Movie seeds"
+
+api_data["results"].each do |hash|
+  Movie.create(
+    title: hash["title"],
+    overview: hash["overview"],
+    poster_url: hash["poster_path"],
+    rating: hash["vote_average"]
+  )
+end
+
+puts "Movie seeds planted!"
+
